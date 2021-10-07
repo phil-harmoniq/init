5ca0bb4e2f369b4b9249adbf2a4b711905aa8e2e
Phil Hawkins <ph.hawkins@outlook.com>
2021-10-07 16:32:04 -0400

    Start over

---
+++ rpi/install-dotnet-5.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl -SL -o dotnet.tar.gz https://dotnetcli.blob.core.windows.net/dotnet/Sdk/master/dotnet-sdk-latest-linux-arm64.tar.gz
+sudo mkdir -p /usr/share/dotnet
+sudo tar -zxf dotnet.tar.gz -C /usr/share/dotnet
+sudo ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
