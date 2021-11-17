d01f5b471fcdd0b689c67c429ec5766aea7d853d
Phil Hawkins <ph.hawkins@outlook.com>
2021-11-17 12:40:38 -0500

    Restructure

--- windows/PoshSetup.ps1
+++ windows/PoshSetup.ps1
@@ -1,4 +1,4 @@
-$Prompt = 'oh-my-posh --init --shell pwsh --config "~\git\init\windows\blueish-custom.omp.json" | Invoke-Expression'
+$Prompt = 'oh-my-posh --init --shell pwsh --config ~\git\init\oh-my-posh\blueish-custom.omp.json | Invoke-Expression'
 
 winget install JanDeDobbeleer.OhMyPosh --accept-package-agreements
 New-Item -Path $Profile -Type file
