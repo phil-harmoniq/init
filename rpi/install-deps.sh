581be074277612dd929f93ee76ffe257d0553d62
Phil Hawkins <ph.hawkins@outlook.com>
2021-12-04 23:14:28 -0500

    Revert "Remove dotnet script"
    This reverts commit 4f40c2c241deb7a3c3ae20efaa0c2220c18b231d.

--- rpi/install-deps.sh
+++ rpi/install-deps.sh
@@ -22,16 +22,17 @@ install_apt_packages()
 install_powershell()
 {
     # https://docs.microsoft.com/en-us/powershell/scripting/install/install-other-linux?view=powershell-7.2#installation-using-a-binary-archive-file
-    curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-x64.tar.gz
-    sudo mkdir -p /opt/microsoft/powershell/7
-    sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
-    sudo chmod +x /opt/microsoft/powershell/7/pwsh
-    sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+    curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-arm64.tar.gz
+    mkdir -p /opt/microsoft/powershell/7
+    tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+    chmod +x /opt/microsoft/powershell/7/pwsh
+    ln -sf /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 }
 
 install_dotnet()
 {
     curl -L -o /tmp/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
+    chmod +x /tmp/dotnet-install.sh
     /tmp/dotnet-install.sh -c 6.0
 }
 
--- ubuntu-arm64/install-powershell-7.2.sh
+++ ubuntu-arm64/install-powershell-7.2.sh
@@ -7,10 +7,10 @@ if [ "$(id -u)" != 0 ]; then
     exit 1
 fi
 
-# https://docs.microsoft.com/en-us/powershell/scripting/install/install-other-linux?view=powershell-7.2#binary-archives
+# https://docs.microsoft.com/en-us/powershell/scripting/install/install-other-linux?view=powershell-7.1#installation-using-a-binary-archive-file
 
 # Download the powershell '.tar.gz' archive
-curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-x64.tar.gz
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-arm64.tar.gz
 
 # Create the target folder where powershell will be placed
 sudo mkdir -p /opt/microsoft/powershell/7
@@ -22,4 +22,4 @@ sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
 sudo chmod +x /opt/microsoft/powershell/7/pwsh
 
 # Create the symbolic link that points to pwsh
-sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+sudo ln -sf /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
