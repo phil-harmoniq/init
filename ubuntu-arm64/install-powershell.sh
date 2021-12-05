4f40c2c241deb7a3c3ae20efaa0c2220c18b231d
Phil Hawkins <ph.hawkins@outlook.com>
2021-12-04 22:28:07 -0500

    Remove dotnet script

---
+++ ubuntu-arm64/install-powershell.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ "$(id -u)" != 0 ]; then
+    echo "This script requires sudo:"
+    exit 1
+fi
+
+# https://docs.microsoft.com/en-us/powershell/scripting/install/install-other-linux?view=powershell-7.2#binary-archives
+
+# Download the powershell '.tar.gz' archive
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-x64.tar.gz
+
+# Create the target folder where powershell will be placed
+sudo mkdir -p /opt/microsoft/powershell/7
+
+# Expand powershell to the target folder
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+
+# Set execute permissions
+sudo chmod +x /opt/microsoft/powershell/7/pwsh
+
+# Create the symbolic link that points to pwsh
+sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
