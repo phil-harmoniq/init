a54138bff4b74cebf0178c858d90fd5d87846b53
Phil Hawkins <ph.hawkins@outlook.com>
2021-12-04 22:17:47 -0500

    install-deps.sh

---
+++ rpi/install-deps.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ "$(id -u)" != 0 ]; then
+    echo "This script requires sudo:"
+    exit 1
+fi
+
+
+install_powershell()
+{
+    # https://docs.microsoft.com/en-us/powershell/scripting/install/install-other-linux?view=powershell-7.2#installation-using-a-binary-archive-file
+    curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-x64.tar.gz
+    sudo mkdir -p /opt/microsoft/powershell/7
+    sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+    sudo chmod +x /opt/microsoft/powershell/7/pwsh
+    sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+}
+
+install_dotnet()
+{
+    curl -L -o /tmp/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
+    /tmp/dotnet-install.sh -c 6.0
+}
