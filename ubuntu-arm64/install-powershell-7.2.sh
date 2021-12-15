9399506890a7218807b57451296395e850190505
Phil Hawkins <ph.hawkins@outlook.com>
2021-12-14 20:09:51 -0500

    Update powershell install script

--- ubuntu-arm64/install-powershell-7.2.sh
+++ ubuntu-arm64/install-powershell-7.2.sh
@@ -10,7 +10,7 @@ fi
 # https://docs.microsoft.com/en-us/powershell/scripting/install/install-other-linux?view=powershell-7.1#installation-using-a-binary-archive-file
 
 # Download the powershell '.tar.gz' archive
-curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-arm64.tar.gz
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-arm64.tar.gz
 
 # Create the target folder where powershell will be placed
 sudo mkdir -p /opt/microsoft/powershell/7
