82a5036ffe2291d09acb1742c1723fe1f09c622f
Phil Hawkins <ph.hawkins@outlook.com>
2024-09-20 17:40:49 -0400

    Fix RHEL init errors

--- rhel/init.sh
+++ rhel/init.sh
@@ -4,27 +4,40 @@ SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 main()
 {
-    add_shell_config
-    install_posh
+    # add_shell_config
+    install_packages
+    # install_posh
 }
 
 add_shell_config()
 {
-    SOURCE_BASH_CONFIG_DIR="$SCRIPT_DIR/rhel/.bashrc.d"
+    SOURCE_BASH_CONFIG_DIR="$SCRIPT_DIR/.bashrc.d"
     DEST_BASH_CONFIG_DIR="$HOME/.bashrc.d/"
 
+    # echo "$SOURCE_BASH_CONFIG_DIR"
+    # echo "$DEST_BASH_CONFIG_DIR"
+
     mkdir -p "$DEST_BASH_CONFIG_DIR"
-    cp "$SOURCE_BASH_CONFIG_DIR/oh-my-posh/aliases" "$DEST_BASH_CONFIG_DIR"
-    cp "$SOURCE_BASH_CONFIG_DIR/oh-my-posh/prompt" "$DEST_BASH_CONFIG_DIR"
+    cp "$SOURCE_BASH_CONFIG_DIR/aliases" "$DEST_BASH_CONFIG_DIR"
+    cp "$SOURCE_BASH_CONFIG_DIR/prompt" "$DEST_BASH_CONFIG_DIR"
+}
+
+install_packages()
+{
+    sudo dnf install epel-release
+    sudo dnf install git htop dotnet-sdk-8.0 shellcheck fastfetch
 }
 
 install_posh()
 {
-    THEMES_DIR="$HOME/.config/oh-my-posh/themes"
+    SOURCE_THEMES_DIR="$SCRIPT_DIR/../oh-my-posh"
+    DEST_THEMES_DIR="$HOME/.config/oh-my-posh/themes"
+
+    # echo "$THEMES_DIR"
 
     curl -s https://ohmyposh.dev/install.sh | bash -s
-    mkdir -p "$THEMES_DIR"
-    cp "$SCRIPT_DIR/oh-my-posh/blueish-custom.omp.json" "$THEMES_DIR"
+    mkdir -p "$DEST_THEMES_DIR"
+    cp "$SOURCE_THEMES_DIR/blueish-custom.omp.json" "$DEST_THEMES_DIR"
 }
 
 main
